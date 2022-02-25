3c50526c4efb1d6cf689ec8530399aeeb86ef7f3
TerPackZ <100250045+TerPackZ@users.noreply.github.com>
2022-02-25 17:35:10 +0600

    Update vkp.py

--- vkp.py
+++ vkp.py
@@ -1,29 +1,37 @@
-import vk, vk_api, time, colorama
+import vk_api, time,colorama
 from colorama import Fore, Back, Style
 from vk_api import VkUpload
 colorama.init()
 
-banner = """
-██╗░░░██╗██╗░░██╗██████╗░██╗░░██╗░█████╗░████████╗░█████╗░
-██║░░░██║██║░██╔╝██╔══██╗██║░░██║██╔══██╗╚══██╔══╝██╔══██╗
-╚██╗░██╔╝█████═╝░██████╔╝███████║██║░░██║░░░██║░░░██║░░██║
-░╚████╔╝░██╔═██╗░██╔═══╝░██╔══██║██║░░██║░░░██║░░░██║░░██║
-░░╚██╔╝░░██║░╚██╗██║░░░░░██║░░██║╚█████╔╝░░░██║░░░╚█████╔╝
-░░░╚═╝░░░╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝░╚════╝░░░░╚═╝░░░░╚════╝░"""
+priva = [
+"""
+██████╗░██╗░░██╗░█████╗░████████╗░█████╗░███╗░░██╗░█████╗░██╗░░██╗██████╗░██╗░░░██╗████████╗██╗░░██╗░█████╗░
+██╔══██╗██║░░██║██╔══██╗╚══██╔══╝██╔══██╗████╗░██║██╔══██╗██║░██╔╝██╔══██╗██║░░░██║╚══██╔══╝██║░██╔╝██╔══██╗
+██████╔╝███████║██║░░██║░░░██║░░░██║░░██║██╔██╗██║███████║█████═╝░██████╔╝██║░░░██║░░░██║░░░█████═╝░███████║
+██╔═══╝░██╔══██║██║░░██║░░░██║░░░██║░░██║██║╚████║██╔══██║██╔═██╗░██╔══██╗██║░░░██║░░░██║░░░██╔═██╗░██╔══██║
+██║░░░░░██║░░██║╚█████╔╝░░░██║░░░╚█████╔╝██║░╚███║██║░░██║██║░╚██╗██║░░██║╚██████╔╝░░░██║░░░██║░╚██╗██║░░██║
+╚═╝░░░░░╚═╝░░╚═╝░╚════╝░░░░╚═╝░░░░╚════╝░╚═╝░░╚══╝╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝░╚═════╝░░░░╚═╝░░░╚═╝░░╚═╝╚═╝░░╚═╝
+"""
+]
 
-print(Fore.CYAN + banner)
-
-subscribe = input("Подписаться на автора в Telegram? (Y/n) ")
-if subscribe == "y":
+opicanya = [
+"""
+Подписаться на автора в Telegram? (yes/no)
+"""
+]
+print(Fore.GREEN + priva[0])
+print(Fore.RED + opicanya[0])
+vibor = input('-->')
+if vibor == "yes":
     os.system("termux-open-url 'https://t.me/joinchat/RUQ98c7lE_wUXexn'")
-elif subscribe == "n":
-    login = input('Введите логин: ')
-    password = input('Введите пароль: ')
-    album = input('Введите id альбома: ')
-vk_session = vk_api.VkApi(login=login, password=password, app_id='2685278')
+elif vibor == "no":
+	login1 = input('Введите логин от страницы:')
+	password1 = input('Введите пароль от страницы:')
+	album = input('Введите ID альбома:')
+vk_session = vk_api.VkApi(login=login1, password=password1, app_id='2685278')
 vk_session.auth(token_only=True)
 vks = vk_session
 upload = VkUpload(vk_session)
 
 while True:
-    upload.photo(photos="photo.jpg", album_id=album)
+	upload.photo(photos="photo.jpg",album_id=album)
