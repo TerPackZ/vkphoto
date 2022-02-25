e6085ebe08f6fa54f34c47973526c5fdb02a8e99
TerPackZ <100250045+TerPackZ@users.noreply.github.com>
2022-02-25 17:27:05 +0600

    Update vkp.py

--- vkp.py
+++ vkp.py
@@ -13,13 +13,17 @@ banner = """
 
 print(Fore.CYAN + banner)
 
-login1 = input('Введите логин: ')
-password1 = input('Введите пароль: ')
-album = input('Введите id альбома: ')
-vk_session = vk_api.VkApi(login=login1, password=password1, app_id='2685278')
-vk_session.auth(token_only=True)
-vks = vk_session
+subscribe = input("Подписаться на автора в Telegram? (Y/n) ")
+if subscribe == "y":
+    os.system("termux-open-url 'https://t.me/joinchat/RUQ98c7lE_wUXexn'")
+elif subscribe == "n":
+    login = input('Введите логин: ')
+    password = input('Введите пароль: ')
+    album = input('Введите id альбома: ')
+    vk_session = vk_api.VkApi(login=login, password=password, app_id='2685278')
+    vk_session.auth(token_only=True)
+    vks = vk_session
 upload = VkUpload(vk_session)
 
 while True:
-	upload.photo(photos="photo.jpg",album_id=album)
+    upload.photo(photos="photo.jpg", album_id=album)
