01053fa5c9c5800e768e839cc98cabf342d05496
TerPackZ <100250045+TerPackZ@users.noreply.github.com>
2022-02-25 21:10:23 +0600

    Update vkp.py

--- vkp.py
+++ vkp.py
@@ -1,4 +1,4 @@
-import vk_api, time,colorama
+import vk_api, time, colorama
 import os
 import sys
 from colorama import Fore, Back, Style
@@ -25,26 +25,24 @@ print(Fore.CYAN + priva[0])
 print(Fore.CYAN + opicanya[0])
 choose = input('--> ')
 if choose == "yes":
-	os.system("termux-open-url 'https://t.me/TerPackZ'")
-	login1 = input('Введите логин: ')
-	password1 = input('Введите пароль: ')
-	album = input('Введите id альбома: ')
-vk_session = vk_api.VkApi(login=login1, password=password1, app_id='2685278')
-vk_session.auth(token_only=True)
-vks = vk_session
-upload = VkUpload(vk_session)
+	login1 = input("Введите логин от страницы: ")
+	password1 = input("Введите пароль от страницы: ")
+	album = input("Введите ID альбома: ")
+	vk_session = vk_api.VkApi(login=login1, password=password1, app_id='2685278')
+	vk_session.auth(token_only=True)
+	vks = vk_session
+	upload = VkUpload(vk_session)
+	while True:
+		upload.photo(photos="photo.jpg", album_id=album)
 
-while True:
-	upload.photo(photos="photo.jpg",album_id=album)
+else:
+	login1 = input("Введите логин от страницы: ")
+	password1 = input("Введите пароль от страницы: ")
+	album = input("Введите ID альбома: ")
+	vk_session = vk_api.VkApi(login=login1, password=password1, app_id='2685278')
+	vk_session.auth(token_only=True)
+	vks = vk_session
+	upload = VkUpload(vk_session)
 
-elif choose == "no":
-	login1 = input('Введите логин: ')
-	password1 = input('Введите пароль: ')
-	album = input('Введите id альбома: ')
-vk_session = vk_api.VkApi(login=login1, password=password1, app_id='2685278')
-vk_session.auth(token_only=True)
-vks = vk_session
-upload = VkUpload(vk_session)
-
-while True:
-	upload.photo(photos="photo.jpg",album_id=album)
+	while True:
+		upload.photo(photos="photo.jpg", album_id=album)
