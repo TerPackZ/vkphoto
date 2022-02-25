909371b1f032ea9e525cfeff31f5db4717c7e4bf
TerPackZ <100250045+TerPackZ@users.noreply.github.com>
2022-02-25 20:48:55 +0600

    Update vkp.py

--- vkp.py
+++ vkp.py
@@ -29,6 +29,14 @@ if choose == "yes":
 	login1 = input('Введите логин: ')
 	password1 = input('Введите пароль: ')
 	album = input('Введите id альбома: ')
+vk_session = vk_api.VkApi(login=login1, password=password1, app_id='2685278')
+vk_session.auth(token_only=True)
+vks = vk_session
+upload = VkUpload(vk_session)
+
+while True:
+	upload.photo(photos="photo.jpg",album_id=album)
+
 elif choose == "no":
 	login1 = input('Введите логин: ')
 	password1 = input('Введите пароль: ')
