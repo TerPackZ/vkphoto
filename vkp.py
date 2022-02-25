0012f22973b1dcd27b4652a52d85d30331239227
TerPackZ <100250045+TerPackZ@users.noreply.github.com>
2022-02-25 17:31:20 +0600

    Update vkp.py

--- vkp.py
+++ vkp.py
@@ -20,9 +20,9 @@ elif subscribe == "n":
     login = input('Введите логин: ')
     password = input('Введите пароль: ')
     album = input('Введите id альбома: ')
-    vk_session = vk_api.VkApi(login=login, password=password, app_id='2685278')
-    vk_session.auth(token_only=True)
-    vks = vk_session
+vk_session = vk_api.VkApi(login=login, password=password, app_id='2685278')
+vk_session.auth(token_only=True)
+vks = vk_session
 upload = VkUpload(vk_session)
 
 while True:
